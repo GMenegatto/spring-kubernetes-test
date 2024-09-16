c413faaab28779e2d9fef4508809f4b8a2a0c282
GMenegatto <gabrielmenegattogf@hotmail.com>
2024-09-15 22:08:28 -0300

    init

---
+++ src/test/java/com/example/springkubernetetest/SpringkubernetetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkubernetetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkubernetetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
